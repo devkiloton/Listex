0effd0dbe94945bb54481dea8251939eb242d5a1
devkiloton <dev.kiloton@gmail.com>
2021-10-05 12:09:06 -0300

    Passing inputs to the console

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,3 +1,11 @@
-const newTask = document.querySelector('[data-form-button]');
+const createTask = (event) =>{
+    //preventDefault() will clear the default form config that is passed as a parameter; 
+    event.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    console.log(value);
+    input.value = "";
+}
 
-newTask.addEventListener('click', ()=>{console.log('fui clicado')});
+const newTask = document.querySelector('[data-form-button]');
+newTask.addEventListener('click', createTask);
