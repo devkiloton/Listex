c63f0412dae67e411f37e71e4d951414c53d981f
devkiloton <dev.kiloton@gmail.com>
2021-10-05 13:33:51 -0300

    Adding elements to the list

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -5,6 +5,12 @@ const createTask = (event) =>{
     const value = input.value;
     console.log(value);
     input.value = "";
+
+    const task = document.querySelector('[data-task]');
+    const content = `<p class="content">${value}</p>`;
+    //inner.HTML defines a HTML or XML syntax
+    task.innerHTML = content;
+    input.value = "";
 }
 
 const newTask = document.querySelector('[data-form-button]');
