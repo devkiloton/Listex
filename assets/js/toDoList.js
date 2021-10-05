46f0b3df6ea0f83fac9ee927a2d54a3d230d64bf
devkiloton <dev.kiloton@gmail.com>
2021-10-05 16:53:40 -0300

    Adding many elements to the list

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,15 +1,23 @@
 const createTask = (event) =>{
     //preventDefault() will clear the default form config that is passed as a parameter; 
     event.preventDefault();
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
     console.log(value);
     input.value = "";
 
-    const task = document.querySelector('[data-task]');
+    const task = document.createElement('li');
+    //classList.add() will add a CSS class to the task content
+    task.classList.add('task');
+
     const content = `<p class="content">${value}</p>`;
+
     //inner.HTML defines a HTML or XML syntax
     task.innerHTML = content;
+
+    //appendChild will add task content to html as a tag
+    list.appendChild(task);
     input.value = "";
 }
 
