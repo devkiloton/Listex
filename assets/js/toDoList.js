d91672570c018941168b65186f4c9fb7fd728d3f
devkiloton <dev.kiloton@gmail.com>
2021-10-06 01:32:05 -0300

    creating check button

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,3 +1,4 @@
+(()=>{
 const createTask = (event) =>{
     //preventDefault() will clear the default form config that is passed as a parameter; 
     event.preventDefault();
@@ -7,19 +8,42 @@ const createTask = (event) =>{
     console.log(value);
     input.value = "";
 
-    const task = document.createElement('li');
-    //classList.add() will add a CSS class to the task content
-    task.classList.add('task');
+    if(value != "")
+    {
+        const task = document.createElement('li');
+        //classList.add() will add a CSS class to the task content
+        task.classList.add('task');
 
-    const content = `<p class="content">${value}</p>`;
+        const content = `<p class="content">${value}</p>`;
 
-    //inner.HTML defines a HTML or XML syntax
-    task.innerHTML = content;
+        //inner.HTML defines a HTML or XML syntax
+        task.innerHTML = content;
+        
+        task.appendChild(concludeButton());
+        //appendChild will add task content to html as a tag
+        list.appendChild(task);
+        }
+    }
 
-    //appendChild will add task content to html as a tag
-    list.appendChild(task);
-    input.value = "";
-}
+    const newTask = document.querySelector('[data-form-button]');
+    newTask.addEventListener('click', createTask);
 
-const newTask = document.querySelector('[data-form-button]');
-newTask.addEventListener('click', createTask);
+    const concludeButton = () => {
+    const concludeButton = document.createElement('button');
+
+    concludeButton.classList.add('check-button');
+    concludeButton.innerText = '✓';
+    concludeButton.addEventListener('click', concludeTask);
+
+    return concludeButton;
+    }
+
+    const concludeTask = (event) =>{
+        const concludeButton = event.target;
+
+        const completeTask = concludeButton.parentElement;
+
+        completeTask.classList.toggle('done');
+    }
+})();
+
